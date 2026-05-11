11cd72d65cf0b4481a1b518fb9ac988ad77c26e2
Amitesh Bathole <amiteshbathole@gmail.com>
2026-05-12 00:48:47 +0530

    changes

--- server.js
+++ server.js
@@ -4,17 +4,28 @@ import cors from 'cors';
 import authRoutes from './router/auth.js';
 import appointmentRoutes from './router/appointment.js';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGO_URI)
+app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.connect(process.env.MONGO_URI);
 
 app.use('/api/auth', authRoutes);
 app.use('/api/appointments', appointmentRoutes);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 if (process.env.NODE_ENV !== 'production') {
     app.listen(process.env.PORT || 3000, () => {
